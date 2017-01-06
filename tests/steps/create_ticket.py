54370b1a1177f7bce14ad50705d964181565d47c
Eddie Dhakal <Eddie Dhakal@localhost>
2017-01-07 08:03:05 +1100

    Wrote the test to creae a valid ticket

--- tests/steps/create_ticket.py
+++ tests/steps/create_ticket.py
@@ -1,4 +1,7 @@
+import json
+
 from behave import *
+import requests
 
 
 @given('the customer has issues with "{issue}"')
@@ -8,4 +11,18 @@ def _(context, issue):
 
 @when('created a ticket with "{issue_type}" as a ticket type')
 def _(context, issue_type):
-    pass
+    request_payload = {
+        "type": "Bug",
+        "message": "The links are broken."
+    }
+
+    response = requests.post(
+        url=context.base_url + '/create-ticket',
+        json=request_payload,
+        headers={'Content-Type': 'application/json'}
+    )
+
+    response_payload = json.loads(response.text)
+    context.ticket_id = response_payload.get('id')
+    assert type(context.ticket_id) is int
+    assert response.ok
