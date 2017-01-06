d98457aa73855b001ec720c4db423fb29f0c6d3c
Eddie Dhakal <Eddie Dhakal@localhost>
2017-01-07 00:36:57 +1100

    Created a basic skeleton to satisfy the environment for feature testing.

---
+++ tests/environment.py
@@ -0,0 +1,25 @@
+import threading
+from wsgiref import simple_server
+
+from handlers import app
+
+
+def clear_database():
+    pass
+
+
+def before_all(context):
+    context.host = 'localhost'
+    context.port = 8000
+    context.base_url = 'http://' + context.host + ':' + str(context.port)
+    context.server = simple_server.make_server(
+        host=context.host,
+        port=context.port,
+        app=app
+    )
+    context.thread = threading.Thread(target=context.server.serve_forever)
+    context.thread.start()
+
+
+def after_all(context):
+    clear_database()
