de30bb4a81decf5f3b2880bd096bb011051e47fd
Eddie Dhakal <Eddie Dhakal@localhost>
2017-01-07 11:26:53 +1100

    Modified the table schema to auto create a ticket ID when it is not passed.

--- ticket.sql
+++ ticket.sql
@@ -1,6 +1,6 @@
 create table "tickets"
 (
-    id int primary key not null,
+    id integer primary key autoincrement,
     ticket_type int not null,
     status int not null,
     message text not null
